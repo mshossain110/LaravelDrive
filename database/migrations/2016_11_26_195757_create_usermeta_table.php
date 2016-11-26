c878fd6519a2cdbab816df1c7929ce0136a3025c
mshossain110 <mshossain110@yahoo.com>
2016-11-27 02:07:03 +0600

    created usermeta table

---
+++ database/migrations/2016_11_26_195757_create_usermeta_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsermetaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usermeta', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned()->index();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('key');
+            $table->string('value');
+            $table->unique(['user_id', 'key']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usermeta');
+    }
+}
