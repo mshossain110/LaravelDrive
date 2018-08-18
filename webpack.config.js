fc2b18adcf3c38ed1ac24c96518181284bb465a9
mshossain110 <mshossain110@gmail.com>
2018-08-18 21:12:11 +0600

    set react env

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path');
+
+/**
+ * required variables
+ */
+var plugins = [];
+
+function adminPath (dir = '') {
+    return path.join(__dirname, './resources/assets/admin/src', dir);
+}
+
+function publicPath (dir = '') {
+    return path.join(__dirname, './public/js', dir);
+}
+
+
+// webpack configaration
+
+module.exports = {
+    entry: {
+        config: adminPath('config/config.js'),
+        admin: adminPath('main.js')
+    },
+
+    output: {
+        filename: '[name].js',
+        path: publicPath(),
+        publicPath: '',
+    },
+
+    resolve: {
+        extensions: ['.js', '.jsx', '.json'],
+        alias: {
+            '@ac' : adminPath('components'),
+            '@ap' : adminPath('pages')
+        }
+    },
+    
+    devtool: 'inline-source-map',
+
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            },
+
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+
+    plugins: plugins
+}
