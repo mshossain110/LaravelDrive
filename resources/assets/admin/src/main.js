9976c45b29f245b37192323815f030cd0d66d223
mshossain110 <mshossain110@gmail.com>
2018-08-21 19:14:46 +0600

    added metarial ui

--- resources/assets/admin/src/main.js
+++ resources/assets/admin/src/main.js
@@ -1,3 +1,18 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import { HashRouter } from "react-router-dom";
+import Layout from '@ac/layout';
 
+
+
+const App = () => {
+    return (
+        <HashRouter>
+            <Layout />
+        </HashRouter>
+    );
+}
+
+
+ReactDOM.render( <App />, document.getElementById('root'));
 
---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+    <div class="laraadmin rootouter">
+        <div id="root"></div>
+    </div>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/config.js') }}" defer></script>
+    <script src="{{ asset('js/admin.js') }}" defer></script>
+</body>
+</html>
